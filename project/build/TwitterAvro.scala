d06bb9bac4e6afccb7a48fd4d6adac614f32ac41
Stephen Tu <stephen_tu@berkeley.edu>
2010-08-15 12:35:33 -0700

    working SBT build

--- project/build/TwitterAvro.scala
+++ project/build/TwitterAvro.scala
@@ -1,4 +1,7 @@
 import sbt._
+import xsbt.ScalaInstance
+
+import java.io.File
 
 class TwitterAvro(info: ProjectInfo) extends DefaultProject(info) with AutoCompilerPlugins
 {
@@ -6,5 +9,15 @@ class TwitterAvro(info: ProjectInfo) extends DefaultProject(info) with AutoCompi
   val avroScala = compilerPlugin("com.googlecode" % "avro-scala-compiler-plugin" % "1.1-SNAPSHOT")
   val pluginRuntime = "com.googlecode" % "avro-scala-compiler-plugin" % "1.1-SNAPSHOT"
   val avro = "org.apache.hadoop" % "avro" % "1.3.3"
+
+  private val pluginDeps = Set("avro-1.3.3.jar", "jackson-core-asl-1.4.2.jar", "jackson-mapper-asl-1.4.2.jar")
+
+  override def getScalaInstance(version: String) = {
+    val pluginJars = compileClasspath.filter(path => pluginDeps.contains(path.name)).getFiles.toSeq
+    withExtraJars(super.getScalaInstance(version), pluginJars) 
+  }
+  
+  def withExtraJars(si: ScalaInstance, extra: Seq[File]) =
+    ScalaInstance(si.version, si.libraryJar, si.compilerJar, info.launcher, extra : _*)
 }
 
